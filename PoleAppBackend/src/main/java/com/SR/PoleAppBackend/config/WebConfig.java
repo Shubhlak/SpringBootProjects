2f4695dd1d67ba55109366cd4ffd7c857978cbd9
Shubhlak <shubhlakkanpate@gmail.com>
2025-04-03 11:23:28 +0530

    second commit

--- PoleAppBackend/src/main/java/com/SR/PoleAppBackend/config/WebConfig.java
+++ PoleAppBackend/src/main/java/com/SR/PoleAppBackend/config/WebConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+	
+	// these is test line 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
